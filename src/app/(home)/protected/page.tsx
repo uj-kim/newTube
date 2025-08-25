a4a7bd475461b4108c7218ad8ff85e1dabafeedf
uj-kim <ninakyj@gmail.com>
2025-08-25 15:07:43 +0900

    feat: 비디오 카테고리 생성 - categories 스키마 생성 - 해당 변경사항 db 반영 - 카테고리 시드 데이터 등록 스크립트 작성 - tRPC 라우터 정리 - 카테고리 컴포넌트 생성

--- src/app/(home)/protected/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div>로그인 유저만 접근할 수 있습니다.</div>;
-};
-
-export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,22 +1,9 @@
-import { z } from "zod";
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { createTRPCRouter } from "../init";
 
 // import { TRPCError } from "@trpc/server";
 export const appRouter = createTRPCRouter({
-  hello: protectedProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      console.log({ dbUser: opts.ctx.user });
-
-      // throw new TRPCError({ code: "BAD_REQUEST"})
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  categories: categoriesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
