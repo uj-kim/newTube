5a3ec60e4e8e76a36ce2495cfe084a15a428ab17
uj-kim <ninakyj@gmail.com>
2025-08-30 17:03:11 +0900

    feat: 무한 스크롤 생성

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,10 +1,32 @@
 "use client";
 
-import { DEFAULT_LIMIT } from "@/constants";
+import { Suspense } from "react";
+import Link from "next/link";
+import { ErrorBoundary } from "react-error-boundary";
 import { trpc } from "@/trpc/client";
+import { DEFAULT_LIMIT } from "@/constants";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const VideosSection = () => {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -12,5 +34,52 @@ export const VideosSection = () => {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     }
   );
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  {/* LegacyBehavior가 없으면 컴포넌트가 깨짐 */}
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>visibility</TableCell>
+                    <TableCell>status</TableCell>
+                    <TableCell>date</TableCell>
+                    <TableCell>views</TableCell>
+                    <TableCell>comments</TableCell>
+                    <TableCell>likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      {/* 무한스크롤은 페이지 외부에 있어도 됨 */}
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
 };
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,7 +1,6 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
