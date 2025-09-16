d4263b2f23b104160e0db7246b745b55d3d9c1bc
uj-kim <ninakyj@gmail.com>
2025-09-16 23:36:51 +0900

    feat: 비디오 상세 페이지 기본 뼈대 만들기(비디오 섹션, 코멘트 섹션, 추천 섹션)

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,6 +1,9 @@
-import { VideoOwner } from "./video-owner";
+import { useMemo } from "react";
+import { format, formatDistanceToNow } from "date-fns";
 import { VideoMenu } from "./video-menu";
+import { VideoOwner } from "./video-owner";
 import { VideoReactions } from "./video-reactions";
+import { VideoDescription } from "./video-description";
 import { VideoGetOneOutput } from "../../types";
 
 interface VideoTopRowProps {
@@ -8,6 +11,24 @@ interface VideoTopRowProps {
 }
 
 export const VideoTopRow = ({ video }: VideoTopRowProps) => {
+  const compactViews = useMemo(() => {
+    return Intl.NumberFormat("en", {
+      notation: "compact",
+    }).format(1245023);
+  }, []);
+  const expandedViews = useMemo(() => {
+    return Intl.NumberFormat("en", {
+      notation: "standard",
+    }).format(1245023);
+  }, []);
+
+  const compactDate = useMemo(() => {
+    return formatDistanceToNow(video.createdAt, { addSuffix: true });
+  }, [video.createdAt]);
+  const expandedDate = useMemo(() => {
+    return format(video.createdAt, "d MMM yyyy");
+  }, [video.createdAt]);
+
   return (
     <div className="flex flex-col gap-4 mt-4">
       <h1 className="text-xl font-semibold">{video.title}</h1>
@@ -18,6 +39,13 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
           <VideoMenu videoId={video.id} variant="secondary" />
         </div>
       </div>
+      <VideoDescription
+        compactViews={compactViews}
+        expandedViews={expandedViews}
+        compactDate={compactDate}
+        expandedDate={expandedDate}
+        description={video.description}
+      />
     </div>
   );
 };
