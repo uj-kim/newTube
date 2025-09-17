bf25cf50982dfff1661ffd2b9445a9c29275cdfd
uj-kim <ninakyj@gmail.com>
2025-09-17 23:54:32 +0900

    feat: 비디오 좋아요/싫어요 반응 구현 - 비디오 리액션 스키마 추가 - video reactions를 getOneVideo 프로시저와 통합 - 좋아요, 싫어요 추가 - video-reactions 컴포넌트를 새 API와 연결

--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -1,31 +1,80 @@
+import { useClerk } from "@clerk/nextjs";
+import { ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { cn } from "@/lib/utils";
-import { ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
+import { VideoGetOneOutput } from "../../types";
+import { toast } from "sonner";
 
-// TODO: 비디오 반응 기능 구현
-export const VideoReactions = () => {
-  const viewerReaction: "like" | "dislike" = "like";
+interface VideoReactionsProps {
+  videoId: string;
+  likes: number;
+  dislikes: number;
+  viewerReaction: VideoGetOneOutput["viewerReaction"];
+}
+
+export const VideoReactions = ({
+  videoId,
+  likes,
+  dislikes,
+  viewerReaction,
+}: VideoReactionsProps) => {
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const like = trpc.videoReactions.like.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+      // TODO: invalidate "liked" playlist 추가
+    },
+    onError: (error) => {
+      toast.error("Something went wrong!");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const dislike = trpc.videoReactions.dislike.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+      // TODO: invalidate "liked" playlist 추가
+    },
+    onError: (error) => {
+      toast.error("Something went wrong!");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
   return (
     <div className="flex items-center flex-none ">
       <Button
+        onClick={() => like.mutate({ videoId })}
+        disabled={like.isPending || dislike.isPending}
         variant="secondary"
         className="rounded-l-full rounded-r-none gap-2 pr-4"
       >
         <ThumbsUpIcon
           className={cn("size-5", viewerReaction === "like" && "fill-black")}
         />
-        {1}
+        {likes}
       </Button>
       <Separator orientation="vertical" className="h-7" />
       <Button
+        onClick={() => dislike.mutate({ videoId })}
+        disabled={like.isPending || dislike.isPending}
         variant="secondary"
         className="rounded-l-none rounded-r-full gap-2 pl-3"
       >
         <ThumbsDownIcon
-          className={cn("size-5", viewerReaction !== "like" && "fill-black")}
+          className={cn("size-5", viewerReaction === "dislike" && "fill-black")}
         />
-        {1}
+        {dislikes}
       </Button>
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
+import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 
 import { createTRPCRouter } from "../init";
 
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   videos: videosRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
+  videoReactions: videoReactionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
