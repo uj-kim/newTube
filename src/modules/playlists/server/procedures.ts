6cffaa2f249af7f3ccb5895a52aa5330f852035c
uj-kim <ninakyj@gmail.com>
2025-09-26 16:02:21 +0900

    feat: 커스텀 플레이리스트 기능 - playlists 스키마 생성 - playlists procedures 생성 - playlists 페이지 생성

--- src/modules/playlists/server/procedures.ts
+++ src/modules/playlists/server/procedures.ts
@@ -1,11 +1,100 @@
 import { z } from "zod";
 import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 import { db } from "@/db";
+import { TRPCError } from "@trpc/server";
 
-import { users, videoReactions, videos, videoViews } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import {
+  playlists,
+  playlistVideos,
+  users,
+  videoReactions,
+  videos,
+  videoViews,
+} from "@/db/schema";
 
 export const playlistsRouter = createTRPCRouter({
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const { id: userId } = ctx.user;
+      const { cursor, limit } = input;
+
+      const data = await db
+        .select({
+          ...getTableColumns(playlists),
+          videoCount: db.$count(
+            playlistVideos,
+            eq(playlists.id, playlistVideos.playlistId)
+          ),
+          users: users,
+        })
+        .from(playlists)
+        .innerJoin(users, eq(playlists.userId, users.id))
+        .where(
+          and(
+            eq(playlists.userId, userId),
+            cursor
+              ? or(
+                  lt(playlists.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(playlists.updatedAt, cursor.updatedAt),
+                    lt(playlists.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(playlists.updatedAt), desc(playlists.id))
+        // 프론트엔드가 요청한 항목 수보다 항상 1개 더 조회 -> 다음 배치에 추가로 로드할 데이터가 있는지 판단
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit; // 더 많은 데이터가 있음
+
+      // 데이터가 더 있는 경우, 마지막 항목 제거
+      // hasMore가 true라면 추가 데이터 존재 여부를 확인하기 위해 추가했던 요소를 제거.(사용자에게 노출 X)
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      // 데이터가 더 있다면 다음 커서를 마지막 아이템으로 설정
+      // 커서 -> 로드한 데이터의 실제 마지막 항목으로 설정
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
+    }),
+  create: protectedProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ input, ctx }) => {
+      const { name } = input;
+      const { id: userId } = ctx.user;
+
+      const [createdPlaylist] = await db
+        .insert(playlists)
+        .values({ userId, name })
+        .returning();
+
+      if (!createdPlaylist) throw new TRPCError({ code: "BAD_REQUEST" });
+
+      return createdPlaylist;
+    }),
   getLiked: protectedProcedure
     .input(
       z.object({
